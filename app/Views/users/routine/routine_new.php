791f62551caa31a779756ffb94fdfb650ed8909c
narcisaducovschi <narcis.aducovschi@gmail.com>
2026-02-24 17:24:52 +0100

    Routine.php (Estructura y estilos acabados, implementar php)

---
+++ app/Views/users/routine/routine_new.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DMN Fitness | Rutina</title>
+    <?= $this->include('partials/css_links') ?>
+</head>
+
+<body>
+    <?= $this->include('partials/sidebar') ?>
+
+
+
+</body>
+
+</html>
