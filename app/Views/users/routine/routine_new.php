0d0482702c96c2f74242607240f95779f15bc50a
narcisaducovschi <narcis.aducovschi@gmail.com>
2026-02-24 18:00:51 +0100

    a

--- app/Views/users/routine/routine_new.php
+++ app/Views/users/routine/routine_new.php
@@ -12,7 +12,44 @@
     <?= $this->include('partials/sidebar') ?>
 
     <div class="container-new">
-        
+         <!-- Cambiar con PHP -->
+        <form action="/" id="routine-new-form" method="POST">
+            <h3>Día</h3><br>
+            <div class="form-header">
+                <div class="form-subheader">
+                    <label for="muscular_group">Grupo Muscular:</label>
+                    <input type="text" placeholder="Espalda, pierna, pecho, etc...">
+                </div>
+                <button>Enviar</button>
+            </div>
+            <br>
+            <h3>Ejercicios: </h3>
+            <div class="add-exercice-container">
+                <div class="add-exercice-info">
+                    <label for="exercice_name">
+                        Nombre del ejercicio
+                    </label>
+                    <input type="text" id="exercice_name" name="exercice_name" placeholder="Ej: Press banca, dominadas, etc...">
+                    <label for="exercice_name">
+                        Nº de series
+                    </label>
+                    <input type="text" id="exercice_series" name="exercice_series" placeholder="">
+                    <label for="exercice_name">
+                        Nº de repeticiones
+                    </label>
+                    <input type="text" id="exercice_repetitions" name="exercice_repetitions" placeholder="">
+                </div>
+                <div  class="add-exercice-note">
+                    <label for="exercice_notes">Notas (Opcional):</label>
+                    <input type="text" id="exercice_notes" name="exercice_notes">
+                </div>
+            </div>
+            <div class="btn-new-container">
+                <a id="plus"><img src="<?= base_url('assets/img/icons/plus.svg'); ?>"></a>
+                <a id="minus"><img src="<?= base_url('assets/img/icons/minus.svg'); ?>"></a>
+            </div>
+        </form> <!-- Camibar action -->
+
     </div>
 
 
