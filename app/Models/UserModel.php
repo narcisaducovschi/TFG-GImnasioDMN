5443463ec01f99ade6700ac96d84fd1a33218393
narcisaducovschi <narcis.aducovschi@gmail.com>
2026-03-07 14:57:21 +0100

    add: UserModel.php

---
+++ app/Models/UserModel.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UserModel extends Model
+{
+    protected $table = 'users';
+    protected $primaryKey = 'id';
+
+    protected $useAutoIncrement = true;
+    protected $returnType = 'array';
+    protected $useSoftDeletes = false;
+
+    protected $allowedFields = [
+        'email',
+        'password',
+        'nombre',
+        'apellidos',
+        'telefono',
+        'direccion',
+        'direccion_extra',
+        'codigo_postal',
+        'ciudad',
+        'id_suscription',
+        'id_rol',
+        'id_class'
+    ];
+
+    protected $useTimestamps = false;
+
+    protected $validationRules = [
+        'email' => 'required|valid_email',
+        'password' => 'required|min_length[6]',
+        'nombre' => 'required',
+    ];
+
+    protected $validationMessages = [];
+    protected $skipValidation = false;
+}
