3c6b3a8c5070790bb63980c88b58b4cd27487173
narcisaducovschi <narcis.aducovschi@gmail.com>
2026-04-30 17:20:18 +0200

    fix de tareamodel

--- app/Models/TareaModel.php
+++ app/Models/TareaModel.php
@@ -9,5 +9,5 @@ class TareaModel extends Model
     protected $table            = 'tareas';
     protected $primaryKey       = 'id';
     protected $allowedFields    = ['id_usuario', 'descripcion', 'fecha_ejecucion']; 
-    protected $useTimestamps    = true; 
+    protected $useTimestamps    = false; 
 }
